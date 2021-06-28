693981e781348e611ff2d9c3e8b792dc3f935115
andresfgp <64049320+andresfgp@users.noreply.github.com>
2021-06-28 17:56:43 -0500

    Merge branch 'master' of https://github.com/andresfgp/PlatziVideo

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -7,37 +7,50 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
-const App = () => (
+const App = () => {
+  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
+  useEffect(() => {
+    fetch('http://localhost:3000/initalState') //recibe una API
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
 
-  <div className='app'>
-    <Header />
-    <Search />
-    <Categories title='Mi lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories title='Tendencia'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories title='Mas vistos'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Footer />
-  </div>
-);
-
+  return (
+    <div className='app'>
+      <Header />
+      <Search />
+      {videos.mylist.length > 0 && (
+        <Categories title='Mi lista'>
+          <Carousel>
+            {
+            // eslint-disable-next-line react/jsx-props-no-spreading
+              videos.mylist.map((item) => <CarouselItem key={item.id} {...item} />)
+            }
+          </Carousel>
+        </Categories>
+      )}
+      {videos.trends.length > 0 && (
+        <Categories title='Tendencia'>
+          <Carousel>
+            {
+            // eslint-disable-next-line react/jsx-props-no-spreading
+              videos.trends.map((item) => <CarouselItem key={item.id} {...item} />)
+            }
+          </Carousel>
+        </Categories>
+      )}
+      {videos.originals.length > 0 && (
+        <Categories title='Originales'>
+          <Carousel>
+            {
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              videos.originals.map((item) => <CarouselItem key={item.id} {...item} />)
+            }
+          </Carousel>
+        </Categories>
+      )}
+      <Footer />
+    </div>
+  );
+};
 export default App;
